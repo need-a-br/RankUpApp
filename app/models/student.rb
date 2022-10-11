9a1f7a5214fa22c331c39f53e8ce5eb25bb2cfb1
sanjuel <samueljscott101@gmail.com>
2022-10-11 13:27:20 -0700

    some validation testing done

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
     belongs_to :user
     validates :name, presence: true
     validates :name, uniqueness: true
+    validates :notes, presence: true
+    validates :rank, presence: true
+    validates :image, presence: true
+    validates :user_id, presence: true
     end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,13 +3,29 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
   let (:user) { User.create email: "sam@test.com",  password: "123456", password_confirmation: "123456"
   }
-  it "is not valid without a name" do
-  scully= Student.create notes: "X-files rocks!", rank: "Alien", image: "https://asdf.jpg", user_id: user.id
-  expect(scully.errors[:name]).to_not be_empty
+    it "is not valid without a name" do
+    scully= Student.create notes: "X-files rocks!", rank: "Alien", image: "https://asdf.jpg", user_id: user.id
+    expect(scully.errors[:name]).to_not be_empty
   end
-  it "does not allow duplicate names" do
-  Student.create(name:"Elle Woods", rank:"Very High", image:"https://i.insider.com/5f08ce4baee6a82702176d61?width=700", user_id:user.id)
-  scully= Student.create(name:"Elle Woods", rank:"Very High", image:"https://i.insider.com/5f08ce4baee6a82702176d61?width=700", user_id:user.id)
-  expect(scully.errors[:name]).to_not be_empty
+    it "does not allow duplicate names" do
+    Student.create(name:"Elle Woods", rank:"Very High", image:"https://i.insider.com/5f08ce4baee6a82702176d61?width=700", user_id:user.id)
+    scully= Student.create(name:"Elle Woods", rank:"Very High", image:"https://i.insider.com/5f08ce4baee6a82702176d61?width=700", user_id:user.id)
+    expect(scully.errors[:name]).to_not be_unique
   end
+  it "is not valid without notes" do
+    scully= Student.create name:"Elle Woods", rank: "Alien", image: "https://asdf.jpg", user_id: user.id
+    expect(scully.errors[:notes]).to_not be_empty
+    end
+  it "is not valid without user_id" do
+    scully= Student.create name:"Elle Woods", rank: "Alien", image: "https://asdf.jpg"
+    expect(scully.errors[:user_id]).to_not be_empty
+    end
+  it "is not valid without rank" do
+    scully= Student.create name:"Elle Woods", image: "https://asdf.jpg", user_id: user.id
+    expect(scully.errors[:rank]).to_not be_empty
+    end
+  it "is not valid without image" do
+    scully= Student.create name:"Elle Woods", rank: "Alien", user_id: user.id
+    expect(scully.errors[:image]).to_not be_empty
+    end
 end
