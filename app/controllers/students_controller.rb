c04e3b0351704f9e2e59a056a6f8e1981a9a2db4
CathrineC <cjsr.carreon@gmail.com>
2022-10-10 19:55:46 -0700

    style devise

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
     before_action :authenticate_user!
-
+    
     def index
         students = Student.select { |student| student.user_id == current_user.id }
         render json: students
--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -1,24 +1,2 @@
 require 'rails_helper'
 
-RSpec.describe "Students", type: :request do
-  
-  describe "GET /index" do
-    it "gets a list of all students" do
-      user = User.where(email: 'test@example.com', password: 'password', password_confirmation: 'password')
- 
-      Students.create(
-        name: "Daniel LaRusso",
-        notes: "Became a black belt in like 3 days",
-        rank: "Black Belt",
-        image: "https://mediaproxy.salon.com/width/1200/https://media.salon.com/2015/08/daniel-the-bully-karate-kid.jpg",
-         user_id: user.id
-      )
-      get '/students'
-
-      student = JSON.parse(response.body)
-      expect(response).to have_http_status(200)
-      expect(student.length).to eq 1
-    # end
-    end
-  end
-end
