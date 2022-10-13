52c15972d89a46f36f27b703f2b8e7a3e69891ea
sanjuel <samueljscott101@gmail.com>
2022-10-13 11:13:20 -0700

    added aboutus page

--- app/javascript/components/pages/Developers.js
+++ app/javascript/components/pages/Developers.js
@@ -1,11 +1,52 @@
-import React from "react"
+import React from "react";
 
 const Developers = () => {
-    return (
+  return (
     <>
-        <h3>Meet the Developers</h3>
+      <h3>Meet the Developers</h3>
+      <img src="https://avatars.githubusercontent.com/u/106039700?v=4" />
+      <h2>Cathrine</h2>
+      <p>
+        Meet Cathrine, our stellar design lead! Cathrine enjoys making fun of
+        people, collecting erasers and doesn’t suffer fools gladly. Her zoom
+        backgrounds are the stuff of legends.
+      </p>
+      <img src="https://avatars.githubusercontent.com/u/109927262?v=4" />
+      <h2>Sam</h2>
+      <p>
+        Meet Sam, our product manager who keeps our app on track! An avid kung
+        fu practitioner who practices and teaches kung fu on a daily basis, Sam
+        collects Lego, like monkeys and hopes to live a secluded mountain life
+        with his beloved girlfriend.
+      </p>
+      <img src="https://avatars.githubusercontent.com/u/109927272?v=4" />
+      <h2>Vanessa</h2>
+      <p>
+        Meet Vanessa, our project manager extraordinaire! She keeps us on track
+        and on time. This marketer turned developer loves to make clothes and
+        cake, often asks questions before realizing she knows the answer, and is
+        irritatingly grateful
+      </p>
+      <img src="https://avatars.githubusercontent.com/u/102928549?v=4" />
+      <h2>Clifford</h2>
+      <p>
+        Meet Cliffie (boy, does he wish you would call him Ford!), our tech
+        lead. When he’s not bossing us around, he loves jamming to Fergalicious,
+        Brunch, Banana Bread and Seth Rogan. Ask him to tell you the story about
+        jelly beans.
+      </p>
+      <p>
+        Our team is composed of Cathrine Carreon, Samuel Scott, Clifford Roecker
+        and Vanessa Bastien. When tasked with the job of meeting Kung Fu
+        instructors’ need for a better way to track their students achievements
+        and goals, the ideas started swirling! Steadily, this team worked
+        through goals and blockers to create the finished product before you.
+        Each member used their strengths and skills and gained a few new ones,
+        too. They proudly present their work, and welcome comments and
+        suggestions on improvements in the future.
+      </p>
     </>
-    )
-}
+  );
+};
 
-export default Developers
+export default Developers;
--- app/javascript/components/pages/ProtectedStudentIndex.js
+++ app/javascript/components/pages/ProtectedStudentIndex.js
@@ -1,66 +1,65 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from "react";
 import {
   Card,
   CardBody,
   CardTitle,
   CardText,
-  ListGroup,
   ListGroupItem,
-  Button,
-
-  CardImg,
   CardImgOverlay,
   Row,
-  Col,
   Container,
   CardImg,
-  CardGroup
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
-const ProtectedStudentIndex = ({readStudents, students}) => {
-  
+const ProtectedStudentIndex = ({ readStudents, students }) => {
   useEffect(() => {
-    readStudents()
-  }, [])
+    readStudents();
+  }, []);
 
   return (
     <>
       <h1>List of Your Students</h1>
-      
-         <div>
-      <Container className="student_cards" >
-      <Row xs={1} md={3}>
-      
-      {students?.map((student, index) => {
-        return (
-                <Card body className="my-2" key={index} style={{maxWidth: 250}}>
-             <CardImgOverlay style={{ height: "18rem",}}>
-          {student.is_ready_for_eval === true && (
-            <CardImg src="../photos/test.png"/>
-          )}
-          </CardImgOverlay>
-            <CardImg alt="Card" src={student.image} />
-            <CardBody>
-              <CardTitle tag="h5">{student.name}</CardTitle>
-              <CardText>Rank: {student.rank}</CardText>
-            </CardBody>
-              <ListGroupItem>
-                Notes: {student.notes}
-              </ListGroupItem>
-            <CardBody>
-              <NavLink to={`/studentshow/${student.id}`} className="nav-link">
-              <img src="photos/Full_Info.png" style={{height: 50}}/>
-              </NavLink>
-            </CardBody>
-          </Card>
-        );
-      })}
-      </Row>
 
-      </Container>
-    </div>
-
+      <div>
+        <Container className="student_cards">
+          <Row xs={1} md={3}>
+            {students?.map((student, index) => {
+              return (
+                <Card
+                  body
+                  className="my-2"
+                  key={index}
+                  style={{ maxWidth: 250 }}
+                >
+                  <CardImgOverlay style={{ height: "18rem" }}>
+                    {student.is_ready_for_eval === true && (
+                      <CardImg src="../photos/test.png" />
+                    )}
+                  </CardImgOverlay>
+                  <CardImg alt="Card" src={student.image} />
+                  <CardBody>
+                    <CardTitle tag="h5">{student.name}</CardTitle>
+                    <CardText>Rank: {student.rank}</CardText>
+                  </CardBody>
+                  <ListGroupItem>Notes: {student.notes}</ListGroupItem>
+                  <CardBody>
+                    <NavLink
+                      to={`/studentshow/${student.id}`}
+                      className="nav-link"
+                    >
+                      <img
+                        src="../photos/Full_Info.png"
+                        style={{ height: 50 }}
+                      />
+                    </NavLink>
+                  </CardBody>
+                </Card>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
